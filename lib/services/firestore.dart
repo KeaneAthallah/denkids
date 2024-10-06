dc70dac0c39ba5c815a462d4b8cebad62ae501bf
Keane Athallah <keaneathallah960@gmail.com>
2024-10-06 18:36:17 +0800

    add validation if name already exist, update the rest

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -4,7 +4,18 @@ class FirestoreService {
   final CollectionReference quizCollection =
       FirebaseFirestore.instance.collection('quiz_results');
 
-  Future<void> addQuizResult(Map<String, dynamic> quizData) {
-    return quizCollection.add(quizData);
+  Future<void> addQuizResult(Map<String, dynamic> quizData) async {
+    final quizCollection =
+        FirebaseFirestore.instance.collection('quiz_results');
+    final querySnapshot =
+        await quizCollection.where('name', isEqualTo: quizData['name']).get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      // Update the existing document
+      await quizCollection.doc(querySnapshot.docs[0].id).update(quizData);
+    } else {
+      // Add a new document
+      await quizCollection.add(quizData);
+    }
   }
 }
